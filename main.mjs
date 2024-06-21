01d21aecb1ab358d2eb272473379dedea19f4a8c
Daniel <hnnk@qq.com>
2024-06-21 15:35:33 +0800

    init

---
+++ main.mjs
@@ -0,0 +1,69 @@
+import * as netmask from "netmask"
+import fs from "node:fs";
+import util from "node:util";
+
+import readline from "node:readline"
+
+import child_process from "node:child_process";
+
+
+let gateway = "172.24.1.1"
+let metric = 499;
+let interfaceIndex = 7;
+
+//  https://github.com/gaoyifan/china-operator-ip/blob/ip-lists/china.txt
+// https://nodejs.org/docs/latest-v18.x/api/readline.html#example-read-file-stream-line-by-line
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('china.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  // 注意：使用 crlfDelay 选项
+  // 将 input.txt 中的所有 CR LF ('\r\n') 实例识别为单个换行符。
+
+  let exec = child_process.exec;
+  let execPromised = util.promisify(exec);
+
+  execPromised(`route change 0.0.0.0 mask 128.0.0.0 192.168.48.1 METRIC 599`)
+  execPromised(`route change 128.0.0.0 mask 128.0.0.0 192.168.48.1 METRIC 599`)
+
+  let num = 0;
+  let str = "@echo off \r\n";
+  for await (const line of rl) {
+    num += 1;
+    console.log("当前行"+ num)
+    // input.txt 中的每一行都将在此处作为 `line` 连续可用。
+    // console.log(`Line from file: ${line}`);
+    const block = new netmask.Netmask(line);
+    // console.log(block.base,block.first, block.mask)
+    try {
+
+      // let r = await execPromised(`route add ${block.base} MASK ${block.mask} ${gateway} METRIC ${metric} IF ${interfaceIndex}`)
+      // let r = execPromised(`route add ${block.base} MASK ${block.mask} ${gateway} METRIC ${metric}`)
+      let r = execPromised(`route delete ${block.base} ${gateway} `)
+
+      // str += `route add ${block.base} MASK ${block.mask} ${gateway} METRIC ${metric} &  `
+      // str += `route delete ${block.base} ${gateway}  &  `
+
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+  str = str.substring(0, str.length - 3)
+  str += `\r\npause`
+  // fs.writeFileSync('route.bat', str)
+}
+
+processLineByLine().then(r =>{
+
+}).finally(r =>{
+  process.exit(0);
+});
+
+
+
+
+// console.log(block)
